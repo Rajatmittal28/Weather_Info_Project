0c90043080b7b2b1b5e603b5700df2fa9af89c26
Rajatmittal28 <57307495+Rajatmittal28@users.noreply.github.com>
2021-09-27 14:39:59 +0530

    improved

--- app.js
+++ app.js
@@ -16,16 +16,30 @@ app.post("/",function(req,res){
     const apikey = process.env.API_KEY;
     const url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&APPID="+`${apikey}`;
     https.get(url,function(response){
-        // console.log(response);
         response.on("data",function(data){
             const weatherData = JSON.parse(data);
-            const temp = Number(weatherData.main.temp)-273;
+            const temp = Math.floor(Number(weatherData.main.temp)-273);
             const weatherDescription = weatherData.weather[0].description;
             const icon = weatherData.weather[0].icon;
             const imageUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-            res.write("<h1> The Weather is currently "+ weatherDescription+"</h1>");
-            res.write("<h2> The Temperature in "+ query +" is " + temp + " degress in Celsius</h2>");
-            res.write("<img src="+imageUrl+">");
+            const currentTime = new Date().toLocaleTimeString();
+            const maxTemp = Math.floor(Number(weatherData.main.temp_max)-273);
+            const minTemp = Math.floor(Number(weatherData.main.temp_min)-273);
+            const humidity = weatherData.main.humidity;
+            const pressure = weatherData.main.pressure;
+            const visibility = weatherData.visibility;
+            const windSpeed = weatherData.wind.speed;
+            const windDirn = weatherData.wind.deg;
+            
+            res.write("<h1>Weather Description is as following</h1>");
+            res.write("<h3>The Weather is currently "+ weatherDescription + "<img src="+imageUrl+"> at current time "+ currentTime +"</h3>");
+            res.write("<h3> The Current Temperature in "+ query +" is " + temp + " degress in Celsius</h3>");
+            res.write("<h5>High/Low Temp: " + maxTemp+"/"+minTemp+" degress in Celsius</h5>");
+            res.write("<h5>humidity: "+humidity+" %</h5>");
+            res.write("<h5>pressure: "+pressure+" hPa</h5>");    
+            res.write("<h5>visibility: "+visibility+" km</h5>");
+            res.write("<h5>wind Speed: "+windSpeed+" km/hr</h5>");
+            res.write("<h5>wind dirn: "+windDirn+" deg</h5>");
             res.send();
         });
     });
